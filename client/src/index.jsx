2c8592de3e05cfcd4af6351f525b97c8ed69cef8
Dmitry Borisenko <brdima92@gmail.com>
2023-03-14 14:37:16 +0300

    jwt

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import App from './App';
+import store from './store';
+
+import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <App />,
+  <BrowserRouter>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </BrowserRouter>,
 );
