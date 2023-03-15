cedf081bbd35b24bdeb8029188269fe7cdf5b116
Ilya Ignatov <119001320+Liquid1617@users.noreply.github.com>
2023-03-15 20:44:53 +0800

    chat+time

--- client/src/pages/GamePage/Chat/Messages/Messages.jsx
+++ client/src/pages/GamePage/Chat/Messages/Messages.jsx
@@ -2,9 +2,16 @@ import React from 'react';
 import style from './css/style.module.css'
 
 const Messages = ({message}) => {
+    const currentTime = new Date(Date.now())
+    console.log(currentTime)
     return (
-        <div className={style.eachMessage}>
-            {message}
+        <div className={style.newMessage}>
+            <div className={style.eachMessage}>
+                {message.text}
+            </div>
+            <div className={style.time}>
+                {message.time}
+            </div>
         </div>
     );
 };
