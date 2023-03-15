2f36ecf82a94d703efbd70ddb22efbd5d7a8a776
Dmitry Borisenko <brdima92@gmail.com>
2023-03-15 17:59:39 +0300

    fix

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,7 +5,6 @@ import {ThemeProvider} from '@emotion/react';
 import {CssBaseline} from '@mui/material';
 import {ColorModeContext, useMode} from './theme';
 import Homepage from './pages/Homepage';
-import Leftbar from './global/Leftbar';
 import Topbar from './global/Topbar/Topbar';
 import Signin from './pages/Signin/Signin';
 import Signup from './pages/Signup/Signup';
@@ -66,6 +65,7 @@ function App() {
                             </>
                         )}
                         <Route index element={<Homepage/>}/>
+                        <Route path='gamepage' element={<GamePage/>}/>
                         <Route path="*" element={<div>404</div>}/>
                     </Routes>
                 </main>
--- client/src/theme.js
+++ client/src/theme.js
@@ -159,7 +159,7 @@ export const themeSettings = (mode) => {
             light: colors.grey[100],
           },
           background: {
-            default: '#fcfcfc',
+            default: '#009f92',
           },
         }),
     },
