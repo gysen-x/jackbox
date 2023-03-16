ce8645f375b5e9ec10ae00a438b757fe609ec494
Ilya Ignatov <119001320+Liquid1617@users.noreply.github.com>
2023-03-16 21:04:54 +0800

    userStatus + allMessages

--- client/src/pages/GamePage/Chat/Chat.jsx
+++ client/src/pages/GamePage/Chat/Chat.jsx
@@ -1,67 +1,81 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import style from './css/style.module.css';
 import Messages from './Messages/Messages';
 import CustomButton from '../../../components/CustomButton/CustomButton';
 import CustomInput from '../../../components/CustomInput/CustomInput';
+import {useParams} from "react-router-dom";
 
 function Chat() {
-  const [message, setMessage] = useState({ text: '', time: null });
-  const [allMessages, setAllMessages] = useState([]);
+    // +user
+    const [message, setMessage] = useState({text: '', time: null, user: null});
+    const [allMessages, setAllMessages] = useState([]);
 
-  const currentTime = new Date(Date.now());
-  const hours = currentTime.getHours();
-  const minutes = currentTime.getMinutes();
-  //   const seconds = currentTime.getSeconds();
+    const currentTime = new Date(Date.now());
+    const hours = currentTime.getHours();
+    const minutes = currentTime.getMinutes();
 
-  const scroll = useRef(null);
-  const handleChange = (event) => {
-    setMessage({
-      ...message,
-      [event.target.name]: event.target.value,
-      time: `${hours < 10 ? (`0${hours}`) : (hours)}:${minutes < 10 ? (`0${minutes}`) : (minutes)}`,
-    });
-  };
+    const scroll = useRef(null);
+    const id = useParams()
 
-  useEffect(
-    () => {
-      if (scroll.current) {
-        scroll.current.scrollTop = scroll.current.scrollHeight;
-      }
-    },
-    [allMessages],
-  );
+    const socketRef = useRef(null);
+    const handleChange = (event) => {
+        setMessage({
+            ...message,
+            [event.target.name]: event.target.value,
+            time: `${hours < 10 ? (`0${hours}`) : (hours)}:${minutes < 10 ? (`0${minutes}`) : (minutes)}`,
+        });
+    };
 
-  console.log(message);
 
-  const onSubmitHandle = (event) => {
-    event.preventDefault();
-    if (message.text !== '') {
-      setAllMessages([...allMessages, message]);
-      setMessage({ text: '' });
-    }
-  };
-  console.log(allMessages);
+    //================================
+    useEffect(() => {
+        const response = fetch(`/rooms/${id}/messages`)
+        response
+            .then((res) => res.json())
+            .then((data) => setAllMessages(data))
+            .catch((error) => console.log(error))
+    }, [])
+    //================================
 
-  return (
-    <div className={style.chatDiv}>
-      <div className={style.header}>
-        <div className={style.title}>Online-chat</div>
-      </div>
-      <div ref={scroll} className={style.allMessages}>
-        {allMessages.map((message1) => (message1.text !== '' ? (
-          <Messages key={message.text} message={message} />) : ('')))}
-      </div>
-      <form onSubmit={onSubmitHandle} className={style.messageInputForm}>
-        <CustomInput
-          className={style.chat__input}
-          value={message.text}
-          name="text"
-          onChange={handleChange}
-        />
-        <CustomButton type="submit" color="#fe9e84" title="Send" />
-      </form>
-    </div>
-  );
+    useEffect(
+        () => {
+            if (scroll.current) {
+                scroll.current.scrollTop = scroll.current.scrollHeight;
+            }
+        },
+        [allMessages],
+    );
+
+    console.log(message);
+
+    const onSubmitHandle = (event) => {
+        event.preventDefault();
+        if (message.text !== '') {
+            setAllMessages([...allMessages, message]);
+            setMessage({text: ''});
+        }
+    };
+
+    return (
+        <div className={style.chatDiv}>
+            <div className={style.header}>
+                <div className={style.title}>Online-chat</div>
+            </div>
+            <div ref={scroll} className={style.allMessages}>
+                {allMessages.map((message1) => (message1.text !== '' ? (
+                    <Messages key={message.text} message={message}/>) : ('')))}
+            </div>
+            <form onSubmit={onSubmitHandle} className={style.messageInputForm}>
+                <CustomInput
+                    className={style.chat__input}
+                    value={message.text}
+                    name="text"
+                    onChange={handleChange}
+                />
+                <CustomButton type="submit" color="#fe9e84" title="Send"/>
+            </form>
+        </div>
+    );
 }
 
 export default Chat;
--- client/src/pages/GamePage/GamePage.jsx
+++ client/src/pages/GamePage/GamePage.jsx
@@ -1,22 +1,47 @@
-import React from 'react';
-import { Grid } from '@mui/material';
+import React, {useEffect, useRef, useState} from 'react';
+import io from 'socket.io-client';
+import {Grid} from '@mui/material';
 import style from './css/style.module.css';
 import Chat from './Chat/Chat';
 import StartGamePage from './GameField/StartGamePage/StartGamePage';
 import GameFriendsPage from './GameField/GameFriendsPage/GameFriendsPage';
+import CustomButton from "../../components/CustomButton/CustomButton";
+
+const SERVER_URL = 'http://localhost:3000';
 
 function GamePage() {
-  return (
-    <Grid className={style.gamePage} container spacing={2}>
-      <Grid item xs>
-        <StartGamePage />
-        <GameFriendsPage />
-      </Grid>
-      <Grid item xs={4}>
-        <Chat />
-      </Grid>
-    </Grid>
-  );
+    const [user, setUser] = useState({})
+    const socketRef = useRef(null);
+
+    useEffect(() => {
+        socketRef.current = io(SERVER_URL);
+        socketRef.current.emit('connection');
+        socketRef.current.on('userStatus', (status) => {
+            setUser({...user, status: status});
+        });
+    }, []);
+
+
+    //На выход из комнаты
+    const handleClick = (event) => {
+        const {id} = event.currentTarget.dataset;
+        const token = localStorage.getItem('token');
+        socketRef.current.emit('disconnectRoom', {id, token, user});
+        navigate(`/rooms`);
+    };
+
+
+    return (
+        <Grid className={style.gamePage} container spacing={2}>
+            <Grid item xs>
+                <StartGamePage/>
+                <GameFriendsPage/>
+            </Grid>
+            <Grid item xs={4}>
+                <Chat/>
+            </Grid>
+        </Grid>
+    );
 }
 
 export default GamePage;
