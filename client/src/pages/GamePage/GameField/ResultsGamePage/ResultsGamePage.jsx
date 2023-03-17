6965ada5f4d51a6a9a4276b1582c58eb86a121da
Dmitry Borisenko <brdima92@gmail.com>
2023-03-17 18:42:04 +0300

    votepage

--- client/src/pages/GamePage/GameField/ResultsGamePage/ResultsGamePage.jsx
+++ client/src/pages/GamePage/GameField/ResultsGamePage/ResultsGamePage.jsx
@@ -1,7 +1,29 @@
+import { Avatar } from '@mui/material';
 import React from 'react';
+import style from './style.module.css';
 
 export default function ResultsGamePage() {
   return (
-    <div>ResultsGamePage</div>
+    <div className={style.resultsGamePage__container}>
+      <h1 className={style.resultsGamePage__title}>Results</h1>
+      <div className={style.resultsGamePage__bestPunch}>
+        <h2 className={style.resultsGamePage__bestPunchTitle}>Best punch</h2>
+        <div className={style.resultsGamePage__resultsGamePage__bestPunchBox}>
+          <p className={style.resultsGamePage__resultsGamePage__bestPunchSetup}>Setup</p>
+          <p className={style.resultsGamePage__resultsGamePage__bestPunchPunch}>Punch</p>
+          <p className={style.resultsGamePage__resultsGamePage__bestPunchPoints}>Points</p>
+        </div>
+      </div>
+      <div className={style.resultsGamePage__rating}>
+        <div className={style.resultsGamePage__ratingSecond}>
+          <p className={style.resultsGamePage__ratingSecond}>2</p>
+          <Avatar />
+        </div>
+        <div className={style.resultsGamePage__ratingFirst}>
+          <p className={style.resultsGamePage__second}>1</p>
+          <Avatar />
+        </div>
+      </div>
+    </div>
   );
 }
--- client/src/pages/GamePage/GameField/VoteGamePage/VoteGamePage.jsx
+++ client/src/pages/GamePage/GameField/VoteGamePage/VoteGamePage.jsx
@@ -7,21 +7,26 @@ export default function VoteGamePage() {
   return (
     <div className={style.voteGamePage__container}>
       <h2 className={style.voteGamePage__title}>Vote</h2>
-      <h2 className={style.voteGamePage__subtitle}>VS</h2>
-      <h3 className={style.voteGamePage__setup}>
+      <h2 className={style.voteGamePage__setup}>
         Ребенок не хочет есть мясо. Чем его можно заменить?
-      </h3>
+      </h2>
       <div className={style.voteGamePage__wrapper}>
         <div className={style.voteGamePage__box}>
-          <h4 className={style.voteGamePage__setup}>Punch</h4>
+          <div className={style.voteGamePage__punchBox}>
+            <h4 className={style.voteGamePage__punch}>Собакой</h4>
+          </div>
           <CustomButton
             title="Vote"
+            color="rgb(254, 158, 132)"
           />
         </div>
         <div className={style.voteGamePage__box}>
-          <h4 className={style.voteGamePage__setup}>Punch</h4>
+          <div className={style.voteGamePage__punchBox}>
+            <h4 className={style.voteGamePage__punch}>Селедкой под шубой</h4>
+          </div>
           <CustomButton
             title="Vote"
+            color="rgb(254, 158, 132)"
           />
         </div>
       </div>
